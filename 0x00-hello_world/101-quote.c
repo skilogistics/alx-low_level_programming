aab6cab02930ba976e2ce3261f0c28f23aa08440
skilogistics <contactzen247@gmail.com>
2023-07-14 10:58:30 -0700

    last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Writing about return 1
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

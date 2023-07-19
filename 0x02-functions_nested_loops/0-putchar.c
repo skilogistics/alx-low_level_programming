2cc6c29bd1c03292ff48ae6853fd1373ad09c1e2
skilogistics <contactzen247@gmail.com>
2023-07-18 21:55:42 -0700

    first ride

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Prints "main" followed by a new line.
+ * done by zenos
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}

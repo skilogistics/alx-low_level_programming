f50d20d9b07d05c47475876017ca7b9d81d5e1b4
skilogistics <contactzen247@gmail.com>
2023-07-27 21:52:45 -0700

    the final push

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
 
+	n1 = n;
+
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		n1 = -n;
 	}
-	else
+
+	if (n1 / 10 != 0)
 	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
 		print_number(n1 / 10);
-
+	}
 	_putchar((n1 % 10) + '0');
 }
